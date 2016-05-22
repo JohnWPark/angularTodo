01f73d6905062531983b082ca36ed6904e4b4399
John Park <jwpark417@gmail.com>
2016-05-22 19:30:17 -0400

    Add ng-repeat to iterate over todos array

--- scripts/app.js
+++ scripts/app.js
@@ -3,14 +3,13 @@ angular.module("todoListApp", [])
   $scope.helloWorld = function() {
     console.log("Hello there, this is the helloWorld controller function in the mainCtrl!");
   };
-})
 
-.controller('coolCtrl', function($scope) {
-  $scope.whoAmI = function() {
-    console.log("Hello there, this is the coolCtrl function!");
-  };
+  $scope.todos = [
+    {"name": "clean the house"},
+    {"name": "water the dog"},
+    {"name": "feed the lawn"},
+    {"name": "run"},
+    {"name": "swim"}
+  ]
 
-  $scope.helloWorld = function() {
-    console.log("This is not the main Ctrl!");
-  };
 });
