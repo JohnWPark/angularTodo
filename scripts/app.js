c0821679e5e2b448ccadece767b103d668333393
John Park <jwpark417@gmail.com>
2016-05-24 12:29:40 -0400

    Add delete and save to services and the view

--- scripts/app.js
+++ scripts/app.js
@@ -11,6 +11,14 @@ angular.module("todoListApp", [])
     $scope.todos = response.data;
   });
 
+  $scope.deleteTodo = function(todo, $index) {
+      dataService.deleteTodo(todo);
+      $scope.todos.splice($index, 1);
+  };
+
+  $scope.saveTodo = function(todo) {
+      dataService.saveTodo(todo);
+  };
 })
 
 .service('dataService', function($http) {
@@ -21,5 +29,15 @@ angular.module("todoListApp", [])
   this.getTodos = function(callback) {
     $http.get('mock/todos.json')
     .then(callback);
+  };
+
+  this.deleteTodo = function(todo) {
+    console.log("The " + todo.name + " todo has been deleted!")
+
+  };
+
+  this.saveTodo = function(todo) {
+    console.log("The " + todo.name + " todo has been saved!")
+
   }
 });
